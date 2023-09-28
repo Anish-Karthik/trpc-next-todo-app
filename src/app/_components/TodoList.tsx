ecd910d42f95c4ca1e14c0494db85330fed4f6a4
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-09-28 21:56:54 +0530

    Completed Client Side todo List

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -5,10 +5,55 @@ import { trpc } from '../_trpc/client'
 
 function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch()
+    }
+  });
+  const deleteTodo = trpc.deleteTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch()
+    }
+  });
+  const toggleTodo = trpc.toggleTodo.useMutation();
+
+  const [content, setContent] = React.useState('')
 
   return (
     <div>
-      <div>{JSON.stringify(getTodos.data)}</div>
+      <input
+        type="text"
+        className='border-2 border-gray-200 text-black' 
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button
+        className='border-2 border-gray-200 bg-black'
+        onClick={async () => {
+          if(!content) return
+          addTodo.mutate(content)
+          setContent('')
+        }}
+      >
+        Add Todo
+      </button>
+      <div className='flex flex-col-reverse gap-2 mt-10'>
+        {getTodos.data?.map((todo) => (
+          <div key={todo.id} className='p-5 bg-gray-800 min-w-[700px]'>
+            <div>{todo.title}</div>
+            <input
+              id={`${todo.id}`}
+              type="checkbox"
+              checked={todo.completed}
+              onChange={(e: any) => {
+                toggleTodo.mutate(todo.id);
+              }}
+            />
+            <button onClick={async () => deleteTodo.mutate(todo.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+      
     </div>
   )
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,57 @@
-// import { db } from './db';
+import { trpc } from '@/app/_trpc/client';
 import { publicProcedure, router } from './trpc';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+
+const prisma = new PrismaClient();
  
 export const appRouter = router({
   getTodos: publicProcedure
     .query(async () => {
-      // Retrieve users from a datasource, this is an imaginary database
-      // const users = await db.user.findMany();
-      // return users;
-      return [1,2,3]
+      const todos = await prisma.todo.findMany();
+      return todos;
+    }),
+  addTodo: publicProcedure
+    .input(z.string())
+    .mutation(async (opts) => {
+      const todo = await prisma.todo.create({
+        data: {
+          title: opts.input,
+          completed: false
+        }
+      });
+      return true;
+    }),
+  deleteTodo: publicProcedure
+    .input(z.number())
+    .mutation(async (opts) => {
+      const todo = await prisma.todo.delete({
+        where: {
+          id: opts.input
+        }
+      });
+      return true;
+    }),
+  toggleTodo: publicProcedure
+    .input(z.number())
+    .mutation(async (opts) => {
+      const todo = await prisma.todo.findUnique({
+        where: {
+          id: opts.input
+        }
+      });
+      if (!todo) {
+        throw new Error('Todo not found');
+      }
+      await prisma.todo.update({
+        where: {
+          id: opts.input
+        },
+        data: {
+          completed: !todo.completed
+        }
+      });
+      return todo.completed;
     }),
 });
 
